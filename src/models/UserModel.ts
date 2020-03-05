32432a039a79d87269e4ac102c3db005ffec8e00
Evan Fiordeliso <evan.fiordeliso@gmail.com>
2020-03-05 08:45:37 -0500

    Fix UserModel.ts

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,10 +1,8 @@
 import Model from "./Model";
-import {RawRowData} from "../database/RowData";
 import {Column, Table} from "../decorators/database";
 import {DataTypes} from "../database/Schema";
-import {where} from "../database/BooleanOperations";
 
-@Table((service, channel) => `${service}_${channel}_users`)
+@Table(service => `${service}_users`)
 export default class UserModel extends Model {
     constructor(id: number, service?: string, channel?: string) {
         super(UserModel, id, service, channel);
@@ -18,8 +16,4 @@ export default class UserModel extends Model {
 
     @Column({ datatype: DataTypes.BOOLEAN })
     public ignore: boolean;
-
-    static async findByName(name: string, service?: string, channel?: string): Promise<UserModel|null> {
-        return Model.retrieve(UserModel, service, channel, where().eq("name", name));
-    }
 }
